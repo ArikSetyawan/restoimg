768173da6867c7b6a1b9557383b14849e2b95474
ArikSetyawan <setyawanarik@gmail.com>
2020-04-06 20:29:59 +0700

    coba config postgredb v9

--- app.py
+++ app.py
@@ -3,34 +3,12 @@ from flask_restful import Api, Resource, reqparse
 from peewee import *
 import os, string, random, base64, io
 from PIL import Image
-import psycopg2
 
 # Models
 import urllib.parse
 
-if 'HEROKU' in os.environ:
-    DEBUG = False
-    urllib.parse.uses_netloc.append('postgres')
-    url = urllib.parse.urlparse(os.environ['DATABASE_URL'])
-    DATABASE = {
-        'engine': 'peewee.PostgresqlDatabase',
-        'name': url.path[1:],
-        'user': url.username,
-        'password': url.password,
-        'host': url.hostname,
-        'port': url.port,
-    }
-else:
-    DEBUG = True
-    DATABASE = {
-        'engine': 'peewee.PostgresqlDatabase',
-        'name': 'framingappdb',
-        'user': 'postgres',
-        'password': 'postgres',
-        'host': 'localhost',
-        'port': 5432 ,
-        'threadlocals': True
-    }
+db_proxy = Proxy()
+
 
 
 class BaseModel(Model):
@@ -46,12 +24,31 @@ def create_tables():
 	with database:
 		database.create_tables([image_file])
 
+if 'HEROKU' in os.environ:
+    import urlparse, psycopg2
+    urlparse.uses_netloc.append('postgres')
+    url = urlparse.urlparse(os.environ["DATABASE_URL"])
+    db = PostgresqlDatabase(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
+    db_proxy.initialize(db)
+else:
+    db = SqliteDatabase('persons.db')
+    db_proxy.initialize(db)
 
 app = Flask(__name__)
 api = Api(app)
 
 # config
 app.config['imgdir'] = 'static/img/product'
+
+@app.before_request
+def before_request():
+    g.db = db_proxy
+    g.db.connect()
+
+@app.after_request
+def after_request(response):
+    g.db.close()
+    return response
 
 class index(Resource):
 	def get(self):
@@ -103,5 +100,6 @@ api.add_resource(resource_image_upload, '/api/restokuimage/')
 api.add_resource(index,'/')
 
 if __name__ == "__main__":
+	db_proxy.connect()
 	create_tables()
 	app.run()
